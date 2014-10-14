c480ee747f7f7a470ea53a00799b196b2aeee505
Hunter Chapman <bootcoder@gmail.com>
2014-10-14 01:08:22 -0700

    initial commit - light pseduo

---
+++ Prime-Xtables.rb
@@ -0,0 +1,53 @@
+
+##########################
+###  Hunter T. Chapman ###
+##  									  ##
+#   bootcoder@gmail.com  #
+##########################
+# 				RUBY      		 #
+#  Prime Multiplication  #
+#        Tables          #
+##########################
+
+require 'awesome_print'
+
+##########################
+# 			PSEUDO CODE
+##########################
+=begin
+
+***INPUT***
+
+User Input from CLI after initial 10x10 grid printout
+User can input prime position on x and y axis to find a given prime product
+
+***OUTPUT***
+
+Initial output to STDOUT is a  10x10 grid of integers
+each col and row contains a prime from 2[0] thru 29[10]
+
+if user inputs additional request output their input with the product displayed below
+
+after each output yield a prompt for additional input or exit
+
+*** PSEUDO ***
+
+
+=end
+
+##########################
+# 			INITIAL CODE
+##########################
+
+
+
+##########################
+# 		ACTIONABLE CODE
+##########################
+
+
+
+##########################
+# 			TEST CODE
+##########################
+
